285e9c6b1442425cd2aa032086a7242e45136f5f
Github User Name <you@example.com>
2018-12-16 22:24:36 +0300

    created the tests for my image class at this commit .

--- posts/tests.py
+++ posts/tests.py
@@ -39,3 +39,43 @@ class ProfileTestClass(TestCase):
         self.assertTrue(profiles.bio, 'Now this is the real bio')
 
 
+class ImageTestClass(TestCase):
+    """
+    Test Image class and its functions
+    """
+    def setUp(self):
+        #creating an new profile and saving it
+        self.profile = Profile(bio='This is my bio')
+        self.profile.save_profile()
+        #creating an new image 
+        self.image = Image(post='test.jpg',caption='Fun times with bae', posted_on='Monday', profile=self.profile)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image, Image))
+
+    def test_save_method(self):
+        """
+        Function to test that image is being saved
+        """
+        self.image.save_img()
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0)
+
+    def test_delete_method(self):
+        """
+        Function to test that an image can be deleted
+        """
+        self.image.save_img()
+        self.image.del_img()
+
+    
+    def test_update_method(self):
+        """
+        Function to test that an image's details can be updated
+        """
+        self.image.save_img()
+        new_image = Image.objects.filter(caption='Fun times with bae').update(caption='Fun times')
+        images = Image.objects.get(caption='Fun times')
+        self.assertTrue(images.caption, 'Fun times')
+    
+
