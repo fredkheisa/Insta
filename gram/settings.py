381d73d5887b3528a5f297e6ff2071a4364fc97b
Github User Name <you@example.com>
2018-12-16 21:55:53 +0300

    created the tokens for the application

--- gram/settings.py
+++ gram/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'posts.apps.PostsConfig',
+    'bootstrap3',
+     'tinymce',
+     'friendship',
 ]
 
 MIDDLEWARE = [
