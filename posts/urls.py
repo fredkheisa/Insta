301d35b936885dece43bf18c55a797452d605d2f
Github User Name <you@example.com>
2018-12-16 22:06:29 +0300

    Created and edited my base html file at this commit .

--- posts/urls.py
+++ posts/urls.py
@@ -19,5 +19,5 @@ urlpatterns = [
     url(r'^like/(?P<image_id>\d+)', views.like, name='like'),
 
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+#if settings.DEBUG:
+#   urlpatterns += static("settings.MEDIA_URL, document_root = settings.MEDIA_ROOT")
